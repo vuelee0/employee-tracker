1f7a42be9c022b4f97f251b0658311fc338dae44
Vue Lee <vuelee09@gmail.com>
2022-06-07 11:55:46 -0500

    initial project files setup

---
+++ db/db.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS employeeData
+
+CREATE DATABASE employeeData;
+USE employeeData;
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const db = require('./db/connection');
+
+
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
